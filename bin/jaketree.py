e589dad0e1d6e7da289902f98d1498bd5f07729a
Jake Boeckerman <jakebman@gmail.com>
2022-08-25 22:49:12 -0600

    figure out the exit issue (forcing the flush was it), and removed all diagnostics

--- bin/jaketree.py
+++ bin/jaketree.py
@@ -3,13 +3,21 @@
 from fileinput import input
 from os.path import commonpath, commonprefix
 import argparse
+import re
 import sys # for argv
 
 def common_word_barrier(strings):
-    return ""
+    prefix = commonprefix(strings)
+    index = len(prefix)
+    l=min(len(s) for s in strings)
+    # if index == l: return prefix # no need to check deeper - they're either all the same, or one is exactly the common prefix
+
+    # TODO: potentially find if the prefix is a barrier for some and not others?
+    # Alternatively, figure out the last-enough barriers in each and pick the (one that is most common? last shared one? maximum?)
+    matches = re.finditer('\\b', prefix)
+    return list(matches)
 
 DEFAULT_COMMON=common_word_barrier
-
 
 def calc_args(argv):
     args = argparse.ArgumentParser(description='Remove common prefixes between lines of input. Works best on already-sorted input')
@@ -19,14 +27,13 @@ def calc_args(argv):
     args.add_argument("--textwise -t", action='store_const', dest='commonStrat', const=commonprefix, help="recognize any text as the common prefix")
     args.set_defaults(commonStrat=DEFAULT_COMMON)
     out = args.parse_args(argv)
-    print(out)
     return out.__dict__
 
 def run_on_files(files=None, commonStrat=DEFAULT_COMMON):
     with input(files=files) as stdin:
         run(stdin, commonStrat=commonStrat)
 
-def run(stdin, commonStrat=commonprefix):
+def run(stdin, commonStrat=DEFAULT_COMMON):
     prev=''
     for line in stdin:
         line = line.strip()
@@ -38,14 +45,12 @@ def do_line(prev, line, commonStrat=DEFAULT_COMMON):
     prefix = commonStrat([line, prev])
     index = len(prefix) # with commonpath, this is stopped BEFORE the common slash
     paranoia = commonprefix([line[index:], prev[index:]])
-    if len(paranoia):
-        print ("PARANOIA PAID OFF:'{}'".format(paranoia))
 
     if deserveElipsis(index, line):
         prefix = ' '*(index-3) + '...'
     else:
         prefix = ' ' * index
-    print(prefix + line[index:], flush=True)
+    print(prefix + line[index:])
 
 
 def deserveElipsis(index, line):
