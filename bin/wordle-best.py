47e5934f4e06e056886d7103c62d1bbc6cfff9d3
Jake Boeckerman <jakebman@gmail.com>
2024-04-22 02:19:03 -0600

    score words, try to explain why each is best

--- bin/wordle-best.py
+++ bin/wordle-best.py
@@ -1,11 +1,30 @@
 #! /bin/env python3
 
+from collections import defaultdict
+
+def squared_sum(it):
+    return sum(i*i for i in it)
+
+def score(word, by_key, target):
+    return squared_sum(len(by_key.get(letter)) for letter in word)
+
+def explain_score(word, by_key):
+    return {letter:len(by_key.get(letter)) for letter in word}
 
 def main(words):
     """
         given a set of words on stdin, print the best wordle guess from them
     """
     print(words)
+    by_key = defaultdict(set)
+    for word in words:
+        for letter in word:
+            by_key[letter].add(word)
+
+    for word in sorted(words, key=lambda word: score(word, by_key, len(words)/2)):
+        print(word, explain_score(word, by_key))
+
+
 
 def get_words():
     words=[]
