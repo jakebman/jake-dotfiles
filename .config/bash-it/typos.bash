f8d6e9962b42cef66c8b449c3c57ccb62af419de
Jake Boeckerman <jakebman@gmail.com>
2025-07-07 13:18:27 -0600

    typos

--- .config/bash-it/typos.bash
+++ .config/bash-it/typos.bash
@@ -333,11 +333,13 @@ typo staut status
 typo tatus status
 typo staus status
 typo cstat stat # allow a level of indrection here - this *might* actually want to be `stat`
+typo stst stat  # ditto.
 # NB: `stat` is an existing command. I needed a function to turn zero-arg `stat` into status, not just a simple alias
 # That's why all-but-one typos go to `status`, because they all intended to be a status command, never a `stat`
 typo sta status
 typo st status        # first unique difference from s's status-or-show magic
 typo branche branches # because sometimes I get lazy, apparently
+typo bracnhes branches
 typo gst gstatus
 typo gstat gstatus
 typo staqsh stash
